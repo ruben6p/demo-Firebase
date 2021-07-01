47022f7197ee57738e17bc7bc5e271b16a43ab87
ruben6p <85554053+ruben6p@users.noreply.github.com>
2021-07-01 11:57:50 +0200

    primer demo firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDLQvE8wBFDkEtOJpoe1F1icT1d9AQhkNE",
+    authDomain: "fir-bio-7be85.firebaseapp.com",
+    projectId: "fir-bio-7be85",
+    storageBucket: "fir-bio-7be85.appspot.com",
+    messagingSenderId: "549036199538",
+    appId: "1:549036199538:web:0cdf8f7a3bb6a1056fbceb",
+    measurementId: "G-Y0Q6VK8VSW"
+  }
 };
